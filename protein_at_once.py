8ff32ada04ba7a4b8553df184a5c4d2925d079de
SamarthCodingHub <samarthkittad8088@gmail.com>
2025-03-20 11:18:29 +0530

    Update protein_at_once.py

--- protein_at_once.py
+++ protein_at_once.py
@@ -1,5 +1,43 @@
 import streamlit as st
 import requests
+import pandas as pd
+
+st.set_page_config(
+        page_title="explore protein like no where"
+        page_icon=":dna:",
+        layout="wide"
+        initial_sidebar_state="expanded",
+)
+
+st.markdown(
+    """
+    <style>
+        .reportview-container .main .block-container{
+            max-width: 90%;
+            padding-top: 5rem;
+            padding-right: 5rem;
+            padding-left: 5rem;
+            padding-bottom: 5rem;
+        }
+        img{
+            max-width:40%;
+            margin-bottom:40px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# --- Sidebar ---
+with st.sidebar:
+    st.image(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/RCSB_PDB_logo.svg/2560px-RCSB_PDB_logo.svg.png",
+        width=200,
+    )  # Replace with a direct link to a smaller image
+    st.title("Protein Data Explorer")
+    st.markdown("Explore protein data from RCSB PDB.")
+
+
 
 st.title('Protein at once')
 protein_input = st.text_input('Enter Protein Name or PDB ID:')
